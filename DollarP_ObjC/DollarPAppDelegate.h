ca5d522434da4aef2db1043322cc7c1a31246f49
Felix Raab <fe9lix@googlemail.com>
2012-11-22 16:24:28 +0100

    initial commit

---
+++ DollarP_ObjC/DollarPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DollarPAppDelegate.h
+//  DollarP_ObjC
+//
+//  Created by Felix Raab on 22.11.12.
+//  Copyright (c) 2012 de.ur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DollarPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DollarP_ObjC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DollarP_ObjC
+//
+//  Created by Felix Raab on 22.11.12.
+//  Copyright (c) 2012 de.ur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DollarPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DollarPAppDelegate class]));
+    }
+}
